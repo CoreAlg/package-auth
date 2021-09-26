c9fc24bcb86908c18040213d0ebc25bfe1e3cd21
Mizanur Rahman <mizan3008@gmail.com>
2021-09-26 12:08:59 +0600

    Add tests

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd /var/www/html && rm -rf public/core-cdn
+cd /var/www/html && rm -rf resources/views/vendor/core-auth
+cd /var/www/html && rm -rf config/core-auth.php
+cd /var/www/html && rm -rf resources/lang/en/core-auth.php
+
+cd /var/www/html && composer remove corealg/auth
+cd /var/www/html && composer require corealg/auth
+
+cd /var/www/html && php artisan vendor:publish --provider="CoreAlg\Auth\Providers\CoreAuthServiceProvider"
+
+# php artisan migrate:fresh
+
+cd /var/www/html && php artisan cache:clear
+cd /var/www/html && php artisan view:clear
+cd /var/www/html && php artisan route:clear
+cd /var/www/html && php artisan config:clear
